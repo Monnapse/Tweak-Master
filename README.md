70844febe775ca156733938abcf232004cc44dd8
Monnapse <masonshuber@gmail.com>
2023-09-29 21:32:56 -0600

    model

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tweak Master
  Tweak your properties in-game with ease of ui
+
+Get the model here -> https://www.roblox.com/library/14920397441/TweakMaster
--- src/TweakMaster/init.lua
+++ src/TweakMaster/init.lua
@@ -1,5 +1,22 @@
 --[[
 	Made by Monnapse
+
+	EXAMPLE
+
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+	local TweakMaster = require(ReplicatedStorage.TweakMaster)
+	
+	local TM = TweakMaster.new()
+	local BooleanChanged = TM:AddProperty("Test 123", TweakMaster.Types.Boolean, true)
+	local NumberChanged = TM:AddProperty("Number Test 123", TweakMaster.Types.Number, 0)
+	
+	BooleanChanged:Connect(function(Value: boolean)
+	    print(Value)
+	end)
+	
+	NumberChanged:Connect(function(Value: number)
+	    print(Value)
+	end)
 --]]
 
 --// Services
